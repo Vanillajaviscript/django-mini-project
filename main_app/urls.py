8819e631b4b8ddb33072fd604668ee06963c344a
Javier Delgado <vanillajaviscript@gmail.com>
2022-07-15 14:32:44 -0700

    adds html pages and folders to templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('finchees/', views.finchees_index, name='index'),
+  path('finchees/<int:finch_id>/', views.finchees_detail, name='detail'),
+  path('finchees/create/', views.FinchCreate.as_view(), name='finchees_create'),
+  path('finchees/<int:pk>/update/', views.FinchUpdate.as_view(), name='finchees_update'),
+  path('finchees/<int:pk>/delete/', views.FinchDelete.as_view(), name='finchees_delete'),
+  path('finchees/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch
+from .forms import FeedingForm
 
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name', 'breed', 'description', 'age']
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finchees/'
 
 def home(request):
-  return HttpResponse('<h1>You Are Home...</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finchees_index(request):
+  finchees = Finch.objects.all()
   return render(request, 'finchees/index.html', { 'finchees': finchees })
+
+def finchees_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  feeding_form = FeedingForm()
+  return render(request, 'finchees/detail.html', {
+    'finch': finch, 'feeding_form': feeding_form, 
+  })
+
+def add_feeding(request, finch_id):
+	# create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
