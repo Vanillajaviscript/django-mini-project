7809abebafc5a6fcfb98fd6628cb8d8be5d7e8f5
Javier Delgado <vanillajaviscript@gmail.com>
2022-07-15 14:16:06 -0700

    adds forms and templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+  return HttpResponse('<h1>You Are Home...</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finchees_index(request):
+  return render(request, 'finchees/index.html', { 'finchees': finchees })
