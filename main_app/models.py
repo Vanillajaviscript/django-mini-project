1d3d0619a32de8eea73a683a76390d7477c7e931
Javier Delgado <vanillajaviscript@gmail.com>
2022-07-17 22:31:39 -0700

    adds dropdown functionality to navbar

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from datetime import date
 from django.contrib.auth.models import User
+from uuid import uuid4
 
 MEALS = (
     ('B', 'Breakfast'),
